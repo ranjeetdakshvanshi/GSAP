662b0d202b88f007cb1e8f5a38950f32a269e2c6
|| 𝐑𝐀𝐍𝐉𝐄𝐄𝐓 || <164492985+ranjeetdakshvanshi@users.noreply.github.com>
2025-01-20 15:07:59 +0530

    Update javascripts.js

--- Test01/src/javascripts.js
+++ Test01/src/javascripts.js
@@ -3,7 +3,15 @@ gsap.to("img",{
            duration:1,
            delay:1,
            stagger:0.3,
-           repeat:1,
+           repeat:-1,
            yoyo:1
           })
-          
+          gsap.to("h1",{
+            opacity: 0,
+            duration:2,
+            delay:1,
+            stagger:0.3,
+            repeat:-1,
+            yoyo:1
+           })
+           
