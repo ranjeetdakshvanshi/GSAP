195e479322971fac569c23d99267e8515d01c78f
𝓡𝓐𝓝𝓙𝓔𝓔𝓣 𝓓𝓐𝓚𝓢𝓗 <164492985+ranjeetdakshvanshi@users.noreply.github.com>
2025-01-22 12:36:23 +0000

    GSAP03

--- Test03/src/javascript.js
+++ Test03/src/javascript.js
@@ -1,36 +1,15 @@
-gsap.from("#page1 #box",{
-    y:-100,
-    opacity:0,
-    duration:1,
-    x:-100,
-    delay:1,
-    repeat:-1,
-    scale:0.3,
-    yoyo:0.1
-   })
-gsap.from("#page3 #box",{
-    rotate:360,
-    
-    scrollTrigger:{
-        scroll:"#page3",
-        trigger:"body",
-        scrub:2,
-        scale:0.3,
-
-    }
-
-})
-gsap.to("#page2 h1",{
-    paddingLeft:"50px",
-    transform:"translate(-150%)",
-    scrollTrigger:{
-        trigger:"#page2",
-        scroll:"body",
-        markers:true,
-        start:"top 0%",
-        end:"top -100%",
-        scrub:2,
-        pin:true,
-    }
-
-})
+var path='M 0 100 Q 500 100 990 100';
+var finalPath='M 0 100 Q 500 100 990 100';
+var page2=document.querySelector("#page2");
+page2.addEventListener("mousemove",function(dets){
+    path=`M 0 100 Q ${dets.x} ${dets.y} 990 100`
+      gsap.to("svg path",{
+        attr:{d:path},
+        ease: "elastic.out(1,0.1)",
+             })
+    })
+ page2.addEventListener("mouseleave",function(){
+    gsap.to("svg path",{
+        attr:{d:finalPath}
+      })
+ })
